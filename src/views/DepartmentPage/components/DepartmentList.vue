9425f12866fcc4b61785f7933dc7175fc3eb8176
Zheng <1359276513@qq.com>
2021-06-02 20:13:47 +0800

    添加了部门列表项的默认值

--- src/views/DepartmentPage/components/DepartmentList.vue
+++ src/views/DepartmentPage/components/DepartmentList.vue
@@ -6,9 +6,21 @@
     <el-table :data="deptList" border style="width: 100%">
       <el-table-column prop="deptNo" label="部门编号" width="150"/>
       <el-table-column prop="name" label="部门名称" width="150"/>
-      <el-table-column prop="managerName" label="部门经理" width="150"/>
-      <el-table-column prop="managerEmpNo" label="部门经理工号" width="180"/>
-      <el-table-column prop="managerContact" label="联系方式" width="180"/>
+      <el-table-column prop="managerName" label="部门经理" width="150">
+        <template slot-scope="scope">
+          {{scope.row.managerName==undefined?'未分配':scope.row.managerName}}
+        </template>
+      </el-table-column>
+      <el-table-column prop="managerEmpNo" label="部门经理工号" width="180">
+        <template slot-scope="scope">
+          {{scope.row.managerEmpNo==undefined?'未分配':scope.row.managerEmpNo}}
+        </template>
+      </el-table-column>
+      <el-table-column prop="managerContact" label="联系方式" width="180">
+        <template slot-scope="scope">
+          {{scope.row.managerContact==undefined?'未分配':scope.row.managerContact}}
+        </template>
+      </el-table-column>
       <el-table-column label="操作">
         <template slot-scope="scope">
           <el-button type="primary" size="mini" @click="onDetailClick(scope.row.deptNo)">详情</el-button>
